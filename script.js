cb97d3ffddf189d718fa090dc9cb09d1e1e0d026
saiyadm99 <syedmohammod52@gmail.com>
2022-08-16 18:15:02 +0600

    1.2

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-// select dom elements
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
+// select dom element
+const counterEl = document.getElementById("counter")
+const incrementEl = document.getElementById("increment")
+const decrementEl = document.getElementById("decrement")
 
-// initial state
-const initialState = {
-    value: 0,
-};
+// action identifiers
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
 
-// create reducer function
-function counterReducer(state = initialState, action) {
-    if (action.type === "increment") {
-        return {
-            ...state,
-            value: state.value + 1,
-        };
-    } else if (action.type === "decrement") {
-        return {
-            ...state,
-            value: state.value - 1,
-        };
-    } else {
-        return state;
-    }
+// action creators
+const increment = (value) => {
+	return{
+		type: INCREMENT,
+		payload: value,
+	}
 }
+
+const decrement = (value) => {
+	return{
+		type: DECREMENT,
+		payload: value,
+	}
+}
+
+// initial state 
+const initialState = {
+	value: 0,
+}
+
+//create reduce function
+function counterReducer(state = initialState, action) {
+	if(action.type === INCREMENT) {
+		return {
+			...state,
+			value: state.value + action.payload,
+		};
+	} else if(action.type === DECREMENT) {
+		return {
+			...state,
+			value: state.value - action.payload,
+		}
+	} else {
+		return state;
+	}
+} 
 
 // create store
 const store = Redux.createStore(counterReducer);
 
 const render = () => {
-    const state = store.getState();
-    counterEl.innerText = state.value.toString();
-};
+	const state = store.getState();
+	counterEl.innerText = state.value.toString();
+}
 
 // update UI initially
 render();
@@ -40,13 +59,15 @@ store.subscribe(render);
 
 // button click listeners
 incrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment",
-    });
-});
+	store.dispatch(increment(5));
+})
 
 decrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "decrement",
-    });
-});
+	store.dispatch(decrement(2));
+})
+
+
+
+
+
+
