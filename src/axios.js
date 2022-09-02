3052cedba2f09634c5a8d667c7bf6461c193dd69
saiyadm99 <niloyaali53@gmail.com>
2022-09-02 13:51:58 +0600

    server link updated localhost to heroku

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:9000"
+	baseURL: "https://lwsserver7.herokuapp.com/"
 });
 
 export default axiosInstance;
